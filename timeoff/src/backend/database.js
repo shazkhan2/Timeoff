ae09b556fc4a433615e773f9531869d6a5094119
shazkhan2 <seewardak@hotmail.com>
2024-03-18 13:03:36 +0100

    Database created

---
+++ timeoff/src/backend/database.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+// create connection
+const knex = require("knex")({
+  client: "mysql2", 
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+   // ssl: { rejectUnauthorized: false }, This has to be enabled before deploying.
+    
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+});
+
+// Check that the connection works
+knex.raw("SELECT VERSION()").then(() => {
+  console.log(`Welldone on getting backend connected you legend!`);
+});
+
+module.exports = knex;
