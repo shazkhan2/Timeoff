7964c3b1871855255ce78526912f9b9f3d96d8d2
Srushti Salke <srushtisalke380@gmail.com>
2024-03-20 22:48:17 +0100

    Added Navbar to the front-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './pages/Navbar';
 import Main from './pages/main';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Main/>
     </div>
   );
