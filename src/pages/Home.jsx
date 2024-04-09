37fcb6abc66a92f0e5a63ef8caf62a01622eb35e
Nico <akebra77@gmail.com>
2024-04-09 11:12:42 +0200

    Added styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react';
-import SubmitTeam from '../component/SubmitTeam';
-import { apiPath } from '../api';
-import CreateMember from '../component/CreateMember';
-import Navbar from './Navbar';
-import '../styles/home.css'
-
+import { useState, useEffect } from "react";
+import SubmitTeam from "../component/SubmitTeam";
+import { apiPath } from "../api";
+import Navbar from "./Navbar";
+import "../styles/home.css";
 
 const Home = () => {
   const [teamsDatabase, setTeamsDatabase] = useState([]);
@@ -15,29 +13,38 @@ const Home = () => {
 
   const fetchTeams = async () => {
     try {
-      const url = apiPath('/teams'); 
+      const url = apiPath("/teams");
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error('Failed to fetch teams');
+        throw new Error("Failed to fetch teams");
       }
       const teams = await response.json();
       setTeamsDatabase(teams);
     } catch (error) {
-      console.error('Error fetching teams:', error);
+      console.error("Error fetching teams:", error);
     }
   };
 
   return (
+    <div className="home-conteiner">
+      <Navbar />
+      <section className="main-section">
+        <div className="headers-conteiiner">
+          <h2 className="main-header">Time Off</h2>
+          <h3 className="main-subheader">
+            TimeOff_App is a web application designed to simplify the process of
+            managing vacations and time off for teams within a company. It
+            provides a user-friendly interface for creating teams, managing team
+            members, and tracking their time off.
+          </h3>
 
-    <div className='home-conteiner'>  <Navbar />   
-    <section className="main-section">
-      <h2 className='main-header'>Time Off</h2>
-      <h3 className='main-subheader'>TimeOff_App is a web application designed to simplify the process of managing vacations and time off for teams within a company. It provides a user-friendly interface for creating teams, managing team members, and tracking their time off.</h3>
-     
-      <SubmitTeam teamsDatabase={teamsDatabase} />
-      
-    </section>
-    </div>  
+          <SubmitTeam teamsDatabase={teamsDatabase} />
+        </div>
+        <div className="main-image-conteiner">
+          <img src="/logo.svg" alt="main-image" className="img-main" />
+        </div>
+      </section>
+    </div>
   );
 };
 
